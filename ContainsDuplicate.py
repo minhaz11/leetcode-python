1bb78d3f328db7cf95f900c461aee97a33dab5ea
unknown <minhaz217@gmail.com>
2023-01-25 23:09:51 +0600

    containDuplicates leetcode 217

---
+++ ContainsDuplicate.py
@@ -0,0 +1,9 @@
+def containsDuplicate(nums):
+        hashset = set()
+        for i in nums:
+            if i in hashset :
+                return True
+            else :
+               hashset.add(i)
+        return False
+print(containsDuplicate())
