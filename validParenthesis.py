89ea478c687440ec7f86a8cf27acdcf4fc0c1e42
minhaz11 <32434200+minhaz11@users.noreply.github.com>
2023-02-23 14:36:30 +0600

    leetcode 20

---
+++ validParenthesis.py
@@ -0,0 +1,13 @@
+def isValid(self, s: str) -> bool:
+        stack = []
+        hashmap = {")":"(","]":"[","}":"{"}
+
+        for i in s:
+            if i in hashmap:
+                if stack and stack[-1] == hashmap[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return True if not stack else False
